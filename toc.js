7c5cafdff7c79e2bb17fdb175a147fd3cb9e4e34
abrisco <91817010+abrisco@users.noreply.github.com>
2025-11-25 15:43:39 +0000

    Deploying to gh-pages from @ periareon/rules_verilog@0a7ae5b2305f6fe7afee86613f6bc4283e59eadc 🚀

--- toc.js
+++ toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded affix "><a href="index.html">Introduction</a></li><li class="chapter-item expanded "><a href="verilog.html"><strong aria-hidden="true">1.</strong> verilog</a></li><li class="chapter-item expanded "><a href="verilator.html"><strong aria-hidden="true">2.</strong> verilator</a></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded affix "><a href="index.html">Introduction</a></li><li class="chapter-item expanded "><a href="verilog.html"><strong aria-hidden="true">1.</strong> verilog</a></li><li class="chapter-item expanded "><a href="verilator.html"><strong aria-hidden="true">2.</strong> verilator</a></li><li class="chapter-item expanded "><a href="system_rdl.html"><strong aria-hidden="true">3.</strong> system_rdl</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString().split("#")[0];
         if (current_page.endsWith("/")) {
